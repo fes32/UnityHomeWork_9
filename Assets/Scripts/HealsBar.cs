479e616739b8d86e8e4d1aa5e0f105f9bf807acf
fes <fesss32@yandex.ru>
2020-12-26 23:25:13 +0500

    Init

---
+++ Assets/Scripts/HealsBar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealsBar : MonoBehaviour
+{
+    [SerializeField] private float _duration;
+    [SerializeField] private float _stepChangeHeals;
+
+    private UnityEngine.UI.Slider _healsBar;
+    private float _speed;
+
+    private void OnEnable()
+    {
+        _healsBar =GetComponent<UnityEngine.UI.Slider>();
+        _speed = _stepChangeHeals /_duration ;
+    }
+
+    public  void Increase()
+    {
+        float heals;
+
+        if (_healsBar.value < _healsBar.maxValue)
+        {
+            heals = _healsBar.value + _stepChangeHeals;
+            StartCoroutine( ChangeHeals(heals)); 
+        }
+    }
+
+    public void Decrease()
+    {
+        float heals;
+
+        if (_healsBar.value > _healsBar.minValue)
+        {
+            heals = _healsBar.value - _stepChangeHeals;
+            StartCoroutine(ChangeHeals(heals));
+        }
+    }
+
+    private IEnumerator ChangeHeals(float heals)
+    {
+        while (_healsBar.value!=heals)
+        { 
+            _healsBar.value = Mathf.MoveTowards(_healsBar.value, heals, _speed *Time.deltaTime);
+            yield return null;
+        }
+    }
+}
